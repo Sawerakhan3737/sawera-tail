059f87349fec5958dad53b7f369bc0ef1b08fec4
ABDUL MUQEET SHAH <you@example.com>
2024-11-21 22:29:08 +0500

    first commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,69 +1,97 @@
-import Boy  from '../../public/boy.jpg'
-import Image from 'next/image'
+               "use client";
+import React, { useEffect } from 'react';
+import Image from 'next/image';
+import Profile from '../../public/boy.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const About = () => {
-  return (
-    <div className='bg-black text-white py-20' id='about'>
-    <div className='container mx-auto px-8 md:px-16 lg:px-24'>
-     <h2 className=' font-extrabold  text-4xl text-center mb-12'> About Me</h2></div>
-        <div className='flex flex-col md:flex-row  items-center md:space-x-12'>
-        <Image src={Boy} alt="sawera " className='w-72 h-80 animate-pulse ml-24 rounded object-cover mb-8  md:mb-0'/>
-        <div className='flex-1'>
-          <p className='text-lg mb-8'>
-            I am a passionate web developer focused on creating engaging, user-friendly websites. With a strong background in
-           HTML, CSS, and JavaScript Tailwind, I thrive on turning ideas into seamless digital experiences. I love staying up-to-date with
-            the latest technologies and collaborating with clients to bring their visions to life. Let is work together to build
-             something amazing!</p>
-             <div className='space-y-4 m-24'> 
-             <div className='flex items-center'>
-              <label htmlFor='htmlandcss'  className='w-2/12'>
-               HTML & CSS
-              </label>
-              <div className='grow bg-gray-800 rounded-full h-2.5'>
-              <div className='bg-gradient-to-r from-yellow-200 to-red-500 h-2.5 rounded-full 
-              transform transition-transform duration-300 hover:scale-105 w-10/12'>
- </div> </div> </div>
-             <div className='flex items-center'>
-              <label htmlFor='htmlandcss'  className='w-2/12'>
-               React JS
-              </label>
-              <div className='grow bg-gray-800 rounded-full h-2.5'>
-              <div className='bg-gradient-to-r from-yellow-200 to-red-500 h-2.5 rounded-full 
-              transform transition-transform duration-300 hover:scale-105 w-9/12'>
-              
-               </div></div></div>
-             <div className='flex items-center'>
-              <label htmlFor='htmlandcss'  className='w-1/12'>
-               Node JS
-              </label>
-              <div className='grow bg-gray-800 rounded-full h-2.5'>
-              <div className='bg-gradient-to-r from-yellow-200 to-red-500 h-2.5 rounded-full 
-              transform transition-transform duration-300 hover:scale-105 w-11/12'>
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-out-back',
+      once: true,
+    });
+  }, []);
 
+  return (
+    <div className="bg-black text-white py-20" id="about">
+      <div className="container mx-auto px-6 sm:px-8 md:px-16 lg:px-24">
+        <h2
+          className="font-extrabold text-3xl sm:text-4xl text-center mb-12"
+          data-aos="fade-up"
+        >
+          About Me
+        </h2>
+        
+        <div className="flex flex-col md:flex-row items-center md:space-x-12">
+          
+          <div
+            className="w-60 h-60 sm:w-72 sm:h-80 rounded-lg mb-8 md:mb-0 mx-auto md:mx-0"
+            data-aos="fade-right"
+          >
+            <Image
+          src={Profile}
+              alt="Sawera"
+              width={288}
+              height={320}
+              className="rounded-2xl object-cover"
+              style={{ width: '100%', height: '100%' }}
+            />
+          </div>
+
+          
+          <div className="flex-1 px-4 sm:px-0" data-aos="fade-left">
+            <p className="text-lg mb-8">
+              I am a passionate web developer focused on creating engaging, user-friendly websites. With a strong background in HTML, CSS, JavaScript, and Tailwind, I thrive on turning ideas into seamless digital experiences. I love staying up-to-date with the latest technologies and collaborating with clients to bring their visions to life. {`Let's`} work together to build something amazing!
+            </p>
+            
+            <div className="space-y-4 mb-12">
+              
+              <div className="flex items-center" data-aos="fade-up" data-aos-delay="200">
+                <label htmlFor="htmlandcss" className="w-2/12 sm:w-1/6">HTML & CSS</label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-yellow-200 to-red-500 h-2.5 rounded-full transition-transform duration-300 w-10/12"></div>
+                </div>
               </div>
-             </div>
-             </div>
-             </div>
-             
-             <div className='mt-12 m-24 flex justify-between text-center'>
-              <div>
-                <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-red-500'>1+</h3>
+
+            
+              <div className="flex items-center" data-aos="fade-up" data-aos-delay="400">
+                <label htmlFor="reactjs" className="w-2/12 sm:w-1/6">React JS</label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-yellow-200 to-red-500 h-2.5 rounded-full transition-transform duration-300 w-9/12"></div>
+                </div>
+              </div>
+
+              
+              <div className="flex items-center" data-aos="fade-up" data-aos-delay="600">
+                <label htmlFor="nextjs" className="w-2/12 sm:w-1/6">Next JS</label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-yellow-200 to-red-500 h-2.5 rounded-full transition-transform duration-300 w-9/12"></div>
+                </div>
+              </div>
+            </div>
+
+            
+            <div className="mt-12 mb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-center">
+              <div data-aos="fade-up" data-aos-delay="800">
+                <h3 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-red-500">1+</h3>
                 <p>Years Experience</p>
               </div>
-              <div>
-                <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-red-500'>10+</h3>
-                <p>Project Completed</p>
+              <div data-aos="fade-up" data-aos-delay="1000">
+                <h3 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-red-500">10+</h3>
+                <p>Projects Completed</p>
               </div>
-              <div>
-                <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-red-500'>5+</h3>
+              <div data-aos="fade-up" data-aos-delay="1200">
+                <h3 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-red-500">5+</h3>
                 <p>Happy Clients</p>
               </div>
-              </div>
-              </div>
-               </div>
-       </div>
-       
-  )
-}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,55 +1,56 @@
-import React from 'react'
- const Services=[{
-    id: 1,
-    title:"Web Design",
-    description:"Creating visually impressive and user-friendly web design.",},
-    { id: 2,
-        title:"Frontend Development",
-        description:" Building responsive and interactive interfaces.",},
-        { id: 3,
-            title:"Backend Development",
-            description:" Developing server-side logic and databases.",},
-            { id: 4,
-                title:"Full-stack Development",
-                description:" Combining both frontend and backend development skills.",},
-                { id: 5,
-                    title:"Content Writing",
-                    description:" Writing content for your business and companies.",},
-                    { id: 6,
-                        title:"Digital marketing",
-                        description:"Promote your business with our digital marketing teams.",
-                    },];
+"use client"
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const Services = [
+  { id: 1, title: "Web Design", description: "Creating visually impressive and user-friendly web design." },
+  { id: 2, title: "Frontend Development", description: "Building responsive and interactive interfaces." },
+  { id: 3, title: "Backend Development", description: "Developing server-side logic and databases." },
+  { id: 4, title: "Full-stack Development", description: "Combining both frontend and backend development skills." },
+  { id: 5, title: "Content Writing", description: "Writing content for your business and companies." },
+  { id: 6, title: "Digital Marketing", description: "Promote your business with our digital marketing teams." },
+];
+
 const Service = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,  
+      easing: 'ease-out-back',  
+      once: true,  
+    });
+  }, []);
+
   return (
-    <div className='bg-black text-white py-20' id='services'>
-    <div className='container mx-auto px-8 md:px-16 lg:px-24'>
-     <h2 className=' font-extrabold  text-4xl text-center mb-12'> My Services</h2>
-     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-     {Services.map(Services =>(
-        <div 
-        key={Services.id} className='bg-gray-800 px-6 pb-6  border rounded-lg hover:shadow-lg transform
-        transiton-transform duration-300 hover:scale-105'
-        >
-        <div className='text-right text-2xl font-bold text-transparent bg-clip-text
-        bg-gradient-to-r from-yellow-200 to-red-500'
-        >
-            {Services.id}
-         </div>
-         <h3 className=' mt-2 text-2xl font-bold text-transparent bg-clip-text
-        bg-gradient-to-r from-yellow-200 to-red-500 '>
-            {Services.title}
-         </h3>
-         <p className='mt-2 text-gray-300'>{Services.description}</p>
-         <a href="#" className='mt-4 inline-block  text-yellow-200 hover:text-red-500'> Read More</a>
-
-         </div>
-     ))}
-
-     </div>
+    <div className="bg-black text-white py-20" id="services">
+      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <h2 className="font-extrabold text-4xl text-center mb-12" data-aos="fade-up">
+          My Services
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {Services.map((service) => (
+            <div 
+              key={service.id}
+              className="bg-gray-800 px-6 pb-6 rounded-lg hover:shadow-lg transform transition-transform duration-300 hover:scale-105 hover:border-2 hover:border-customPink"
+              data-aos="fade-up" 
+              data-aos-delay={`${service.id * 100}`}
+            >
+              <div className="text-right text-2xl font-bold text-transparent bg-clip-text  bg-gradient-to-r from-yellow-200 to-red-500">
+                {service.id}
+              </div>
+              <h3 className="mt-2 text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-red-500">
+                {service.title}
+              </h3>
+              <p className="mt-2 text-gray-300">{service.description}</p>
+              <a href="#" className="mt-4 inline-block bg-clip-text bg-gradient-to-r from-yellow-200 to-red-500 text-white font-semibold py-2 px-4 rounded-full transform transition-transform duration-300 hover:scale-105">
+                Read More
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
-     </div>
-    
-  )
-}
+    </div>
+  );
+};
 
-export default Service
+export default Service;
